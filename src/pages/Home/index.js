3b21023bba4b5acb3d613c84732a38f8d28fe281
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-03-06 11:43:28 -0300

    Criação da página de busca

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,31 @@
 import React from "react";
-import Logo from './components/Logo'
 import Input from './components/Input'
-import MainTitle from "./components/MainTitle";
 import SearchButton from "./components/SearchButton";
-import LastSearchButton from "./components/LastSearchButton";
+import main_page_logo from './components/main_page_logo.svg';
 
 import './styles.css'
 
 const Home = () => {
     return(
-        <body className="background"> 
-           <Logo/>
-           <MainTitle/>
-           <Input/>
-           <SearchButton/>
-           <LastSearchButton/>
-        </body>
+        <main className="background">
+            <div className="content">
+                <header className="header_main_page">
+                    <img src={main_page_logo} />
+                    <a href="http://google.com">Últimas buscas</a>
+                </header>
+                <body className="body_main_page">
+                    <div className="main-content">
+                        <div className="Titulo ">
+                            Buscar Letra
+                        </div>
+                        <Input/>
+                        <div className="height_search_button_main_page">
+                            <SearchButton/>
+                        </div>
+                    </div>
+                </body>
+            </div>
+        </main>
     );
 }
 
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,49 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"
+import main_page_logo from './components/main_page_logo.svg';
+import Lupa from './components/Lupa.svg'
+
+import './styles.css'
+
+/* Analisar a responsividade da página -> não está ocorrendo, não sei porque.*/ 
+/* Será, primeiramente, implementado a página de busca de quando o artista encontrou a música. */
+/* Analisar a criação de um componente para o botão. -> desacoplamento */ 
 
 const Search = () => {
-    return(
-        <header>
-            MUSIC NOT FOUND
-        </header>
+    const navigate = useNavigate();
+
+    const goToMenu = () => {
+        navigate('/')
+    }
+
+    return (
+        <main className="background_search1">
+            <div className="content_search1">
+                <header className="logo_search1">
+                    <img src={main_page_logo} />
+                </header>
+                <body className="search1_body">
+                    <div className="main_content_search1">
+                        <div className="text1_search1">
+                            Letra encontrada
+                        </div>
+                        <div className="text2_search1">
+                            Não encontrou o
+                            que procurava?
+                        </div>
+                        <div className="height_button_search1">
+                            <button onClick={() => goToMenu()}
+                                    className="new_search_button_search1">
+                                <img className="lupa_search1" src={Lupa} />
+                                <div className="nova_busca_search1">
+                                    Nova Busca
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </body>
+            </div>
+        </main>
     );
 }
 
